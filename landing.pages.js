161a565fa7770c22260546f76b8444fd3228c476
Joice Vitoria Cidreira <128824664+joicevitoriacidreira@users.noreply.github.com>
2023-09-26 12:15:32 -0300

    Update landing.pages.js

--- landing.pages.js
+++ landing.pages.js
@@ -1,1 +1,31 @@
 
+    document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+        anchor.addEventListener('click', function(e) {
+            e.preventDefault();
+
+            const targetId = this.getAttribute('href').substring(1);
+            const targetElement = document.getElementById(targetId);
+
+            if (targetElement) {
+                window.scrollTo({
+                    top: targetElement.offsetTop,
+                    behavior: 'smooth' // Isso adiciona um efeito de rolagem suave
+                });
+            }
+        });
+    });
+
+   document.addEventListener("DOMContentLoaded", function () {
+    const questions = document.querySelectorAll(".question");
+
+    questions.forEach(function (question) {
+        question.addEventListener("click", function () {
+            const answer = this.nextElementSibling;
+            if (answer.classList.contains("active")) {
+                answer.classList.remove("active");
+            } else {
+                answer.classList.add("active");
+            }
+        });
+    });
+});
